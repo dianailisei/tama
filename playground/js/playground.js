4216bbc2c0f95ce3d5942f8c320a3643f1a19efe
catl4 <cristiana.antalut@gmail.com>
2018-10-31 20:32:43 +0200

    added pet reaction

---
+++ playground/js/playground.js
@@ -0,0 +1,7 @@
+var petContainer = document.getElementById("penguin-down");
+var petReaction = document.getElementById("pet-reaction");
+
+petContainer.addEventListener("click", function(){
+     petReaction.style.visibility = "visible"; 
+
+}, true);
