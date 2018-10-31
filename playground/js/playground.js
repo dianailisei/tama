a0df546f29d0ed205037a45d6601dede71a5cd63
catl4 <cristiana.antalut@gmail.com>
2018-11-01 00:07:56 +0200

    added pet state for playground

--- playground/js/playground.js
+++ playground/js/playground.js
@@ -1,5 +1,6 @@
 var petContainer = document.getElementById("penguin-down");
 var petReaction = document.getElementById("pet-reaction");
+var petState = document.getElementById("pet-state");
 
 function startHeartAnimation(){      
      petReaction.classList.add("pet-reaction-animation");
@@ -10,3 +11,21 @@ function startHeartAnimation(){
           petReaction.classList.remove("pet-reaction-animation");
      }, 5000);
 }
+
+function showPetState(){
+     // petContainer.addEventListener("mouseover", function( event ) {   
+          // highlight the mouseover target
+          // event.target.style.visibility = "hidden";
+
+     // }, false);
+     petState.style.visibility = "visible";
+}
+
+function hidePetState(){
+     // petContainer.addEventListener("mouseout", function( event ) {   
+          // highlight the mouseout target
+          // event.target.style.visibility = "visible";
+     // }, false);
+     petState.style.visibility = "hidden";
+}
+
