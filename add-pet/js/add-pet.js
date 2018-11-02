0e2f2692c69f27fc65f8b6fe3d3761c08690265e
catl4 <cristiana.antalut@gmail.com>
2018-11-02 06:08:12 +0200

    added configuration option add-pet

--- add-pet/js/add-pet.js
+++ add-pet/js/add-pet.js
@@ -1,3 +1,16 @@
+var petColor = document.getElementById("penguin-up-belly");
+var petEyeLeft = document.getElementById("penguin-up-left-eye");
+var petEyeRight =document.getElementById("penguin-up-right-eye");
+
 function saveNewPet(){
      console.log("your new pet is addopted!");
+}
+
+function changePetEyesColor(val){
+     petEyeLeft.style.fill = val;
+     petEyeRight.style.fill = val; 
+}
+
+function changePetColor(val){
+     petColor.style.fill = val;
 }
